e9ed9679ac66d1662fa9874413e8962a86359b7a
DaniilMashkov <dankeeeez@gmail.com>
2023-03-13 22:34:02 +0700

    fixes

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from users.serializers import User, UserSerializer
@@ -9,9 +9,13 @@ from django.contrib.auth.hashers import make_password
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
-        request.data._mutable = True
+
+        """string for tests """
+        # request.data._mutable = True
+
         request.data.update({'password': make_password(request.data.get('password'))})
         super().create(request, *args, **kwargs)
         return Response({"To continue start a chat with bot https://t.me/Suuupsupbot"},
@@ -37,7 +41,3 @@ class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
         if self.request.user.is_staff:
             return User.objects.all()
         return User.objects.get(pk=self.request.user.pk)
-
-
-
-
